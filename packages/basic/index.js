1e8e7fd1caaa6f889ad6ac8f2ffa14788fa7ab2a
anthony <924358746@qq.com>
2022-11-10 21:27:49 +0800

    feat: basic新增ignorePatterns

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,5 +1,23 @@
 module.exports = {
   extends: ["airbnb-base"],
+  ignorePatterns: [
+    '*.min.*',
+    '*.d.ts',
+    'CHANGELOG.md',
+    'dist',
+    'LICENSE*',
+    'output',
+    'coverage',
+    'public',
+    'temp',
+    'package-lock.json',
+    'pnpm-lock.yaml',
+    'yarn.lock',
+    '__snapshots__',
+    '!.github',
+    '!.vitepress',
+    '!.vscode',
+  ],
   rules: {
     'import/prefer-default-export': 'off'
   }
