73841bf46fec63c552bf8f9d6182aa839a76f416
anthony <924358746@qq.com>
2022-11-10 21:23:51 +0800

    feat: 在ts文件中关闭重复的警告

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -10,7 +10,7 @@ module.exports = {
     },
   },
   rules: {
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     // es6
     'prefer-const': 'off',
