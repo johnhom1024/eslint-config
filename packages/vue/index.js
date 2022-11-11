befbe258cd92962ff8afef6bb9e491353d8f4369
anthony <924358746@qq.com>
2022-11-11 16:15:52 +0800

    fix: 修复vue规则中的parser拼写错误

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -2,7 +2,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.vue'],
-      parser: 'vue-eslit-parser',
+      parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
